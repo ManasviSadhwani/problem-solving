b7962d44e50888001a41060dbb4072f93ccd0471
Manasvi Sadhwani <65923458+ManasviSadhwani@users.noreply.github.com>
2022-09-05 21:58:53 +0530

    Reverse-String

---
+++ Recursion/Problem-7.java
@@ -0,0 +1,18 @@
+// LeetCode Link - https://leetcode.com/problems/reverse-string/
+
+class Solution {
+    public static void Helper(char[] s, int l, int r){
+        if(l>=r) return;
+        
+        char temp = s[l];
+        s[l] = s[r];
+        s[r] = temp;
+        
+        Helper(s, l+1, r-1);
+    }
+    
+    public void reverseString(char[] s) {
+        Helper(s, 0, s.length -1);
+        return;
+    }
+}
